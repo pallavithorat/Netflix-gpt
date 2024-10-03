711f768ad78ef63483ab0c50263acb0a25704c23
pallavithorat <thoratpallavi2000@gmail.com>
2024-10-02 21:21:05 -0700

    Added Sign In, Sign Up, Login forms, and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="App text-3xl font-bold text-red-500">
-     Hello Everyone, Let's build netflix-gpt
-    </div>
-  );
+  
+  return <Body />;
 }
 
 export default App;
